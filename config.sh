c09e37f4b27228afa70b3069054292ae0a6f2c31
Subhan Waseem <l1f21bsse0250@ucp.edu.pk>
2025-01-23 11:17:35 +0500

    initial commit

---
+++ config.sh
@@ -0,0 +1,7 @@
+echo "going to configure repo"
+git branch -m develop
+git config --local user.name "Subhan Waseem"
+git config --local user.email l1f21bsse0250@ucp.edu.pk
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
